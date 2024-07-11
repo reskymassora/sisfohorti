1052139b1423647358398659c5f080407af67880
reskymassora <reskymassora@gmail.com>
2024-07-11 20:13:08 +0800

    memperbaiki warna text di sidebar

--- navigation/sidebar.php
+++ navigation/sidebar.php
@@ -3,20 +3,20 @@
     <li class="nav-item">
       <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
       <a class="nav-link <?= $page == 1 ? 'active' : '' ?>" href="dashboard_admin.php">
-        <span class="nav-icon">
+        <span class="nav-icon text-success">
           <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z" />
             <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
           </svg>
         </span>
-        <span class="nav-link-text">Dashboard</span> </a><!--//nav-link-->
+        <span class="nav-link-text text-success">Dashboard</span> </a><!--//nav-link-->
     </li>
 
     <!--//nav-item-->
     <li class="nav-item">
       <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
       <a class="nav-link <?= $page == 2 ? 'active' : '' ?>" href="input.php">
-        <span class="nav-icon">
+        <span class="nav-icon text-success">
           <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-card-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
             <path fill-rule="evenodd" d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z" />
@@ -25,7 +25,7 @@
             <circle cx="3.5" cy="10.5" r=".5" />
           </svg>
         </span>
-        <span class="nav-link-text">Input Data</span> </a><!--//nav-link-->
+        <span class="nav-link-text text-success">Input Data</span> </a><!--//nav-link-->
     </li>
     <!--//nav-item-->
   </ul>
