6ea5d337b68e63c974d5fa3c74631d974c568961
reskymassora <reskymassora@gmail.com>
2024-07-28 19:43:06 +0800

    Mengubah bentuk tombol ubah dan hapus

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -191,12 +191,16 @@ if ($userInfo !== false) {
                           <td class="cell"><?= $data['tanggal'] ?></td>
                           <td class="cell">
                             <a class="btn app-btn-primary p-2" href="edit.php?id=<?= urlencode($data["id"]) ?>">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-</svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                              </svg>
                             </a>
-                            <a class="btn app-btn-secondary deleteButton p-2" href="delete.php?id=<?= urlencode($data['id']) ?>">Hapus</a>
+                            <a class="btn app-btn-secondary deleteButton p-2" href="delete.php?id=<?= urlencode($data['id']) ?>">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                              </svg>
+                            </a>
                           </td>
                         </tr>
                         <?php $i++; ?>
@@ -224,72 +228,72 @@ if ($userInfo !== false) {
   <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 
   <script>
-  function addDeleteConfirmation(buttons) {
-    buttons.forEach(function(button) {
-      button.addEventListener('click', function(event) {
-        event.preventDefault();
-        const deleteUrl = this.href;
+    function addDeleteConfirmation(buttons) {
+      buttons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+          event.preventDefault();
+          const deleteUrl = this.href;
 
-        Swal.fire({
-          title: 'Yakin ingin menghapus data?',
-          text: "Anda tidak akan dapat mengembalikan ini!",
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Ya, hapus!',
-          cancelButtonText: 'Batal'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            // Arahkan ke URL penghapusan jika pengguna mengonfirmasi
-            window.location.href = deleteUrl;
-          }
+          Swal.fire({
+            title: 'Yakin ingin menghapus data?',
+            text: "Anda tidak akan dapat mengembalikan ini!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya, hapus!',
+            cancelButtonText: 'Batal'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              // Arahkan ke URL penghapusan jika pengguna mengonfirmasi
+              window.location.href = deleteUrl;
+            }
+          });
+          // Mengembalikan false untuk mencegah aksi default dari tautan
+          return false;
         });
-        // Mengembalikan false untuk mencegah aksi default dari tautan
-        return false;
       });
-    });
-  }
-
-  // Inisialisasi event listener untuk tombol hapus pada halaman awal
-  addDeleteConfirmation(document.querySelectorAll('.deleteButton'));
-
-  //Search
-  function handleSearch() {
-    const query = document.getElementById('search').value;
-    const mainTable = document.getElementById('main-table');
-    const resultsDiv = document.getElementById('results');
-
-    if (query.length > 2) { // Memulai pencarian jika input lebih dari 2 karakter
-      mainTable.classList.add('hidden'); // Sembunyikan tabel utama
-      mainTable.classList.remove('visible');
-      fetch(`function.php?action=search&q=${encodeURIComponent(query)}`)
-        .then(response => response.json())
-        .then(data => {
-          displayResults(data);
-        })
-        .catch(error => console.error('Error:', error));
-    } else {
-      mainTable.classList.remove('hidden'); // Tampilkan kembali tabel utama
-      mainTable.classList.add('visible');
-      resultsDiv.innerHTML = ''; // Bersihkan hasil pencarian
     }
-  }
 
-  // Event listener untuk input pencarian
-  document.getElementById('search').addEventListener('input', handleSearch);
+    // Inisialisasi event listener untuk tombol hapus pada halaman awal
+    addDeleteConfirmation(document.querySelectorAll('.deleteButton'));
 
-  // Fungsi untuk menampilkan hasil pencarian
-  function displayResults(data) {
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = '';
+    //Search
+    function handleSearch() {
+      const query = document.getElementById('search').value;
+      const mainTable = document.getElementById('main-table');
+      const resultsDiv = document.getElementById('results');
 
-    if (data.length > 0) {
-      const table = document.createElement('table');
-      table.className = 'table app-table-hover mb-0 text-left';
+      if (query.length > 2) { // Memulai pencarian jika input lebih dari 2 karakter
+        mainTable.classList.add('hidden'); // Sembunyikan tabel utama
+        mainTable.classList.remove('visible');
+        fetch(`function.php?action=search&q=${encodeURIComponent(query)}`)
+          .then(response => response.json())
+          .then(data => {
+            displayResults(data);
+          })
+          .catch(error => console.error('Error:', error));
+      } else {
+        mainTable.classList.remove('hidden'); // Tampilkan kembali tabel utama
+        mainTable.classList.add('visible');
+        resultsDiv.innerHTML = ''; // Bersihkan hasil pencarian
+      }
+    }
 
-      const thead = document.createElement('thead');
-      thead.innerHTML = `
+    // Event listener untuk input pencarian
+    document.getElementById('search').addEventListener('input', handleSearch);
+
+    // Fungsi untuk menampilkan hasil pencarian
+    function displayResults(data) {
+      const resultsDiv = document.getElementById('results');
+      resultsDiv.innerHTML = '';
+
+      if (data.length > 0) {
+        const table = document.createElement('table');
+        table.className = 'table app-table-hover mb-0 text-left';
+
+        const thead = document.createElement('thead');
+        thead.innerHTML = `
         <tr>
           <th class="cell">No.</th>
           <th class="cell">Distrik</th>
@@ -302,13 +306,13 @@ if ($userInfo !== false) {
           <th class="cell">Tindakan</th>
         </tr>
         `;
-      table.appendChild(thead);
+        table.appendChild(thead);
 
-      const tbody = document.createElement('tbody');
+        const tbody = document.createElement('tbody');
 
-      data.forEach((item, index) => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
+        data.forEach((item, index) => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
           <td class="cell">${index + 1}</td>
           <td class="cell">${item.distrik}</td>
           <td class="cell">${item.komoditas}</td>
@@ -322,18 +326,18 @@ if ($userInfo !== false) {
             <a class="btn app-btn-secondary deleteButton p-2" href="delete.php?id=${encodeURIComponent(item.id)}">Hapus</a>
           </td>
           `;
-        tbody.appendChild(row);
-      });
-      table.appendChild(tbody);
-      resultsDiv.appendChild(table);
+          tbody.appendChild(row);
+        });
+        table.appendChild(tbody);
+        resultsDiv.appendChild(table);
 
-      // Tambahkan event listener untuk tombol hapus pada hasil pencarian
-      addDeleteConfirmation(document.querySelectorAll('.deleteButton'));
-    } else {
-      resultsDiv.innerHTML = '<p>No results found</p>';
+        // Tambahkan event listener untuk tombol hapus pada hasil pencarian
+        addDeleteConfirmation(document.querySelectorAll('.deleteButton'));
+      } else {
+        resultsDiv.innerHTML = '<p>No results found</p>';
+      }
     }
-  }
-</script>
+  </script>
 
 
   <!-- Charts JS -->
@@ -345,5 +349,3 @@ if ($userInfo !== false) {
 </body>
 
 </html>
-
-
