8563ca3e940dc46e5fff1702f6041f9e51fb815b
reskymassora <reskymassora@gmail.com>
2024-07-28 19:37:01 +0800

    Mengembalikan format tombol

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -190,9 +190,7 @@ if ($userInfo !== false) {
                           <td class="cell">Rp <?= $data['hktppm'] ?></td>
                           <td class="cell"><?= $data['tanggal'] ?></td>
                           <td class="cell">
-                            <a class="btn app-btn-primary p-2" href="edit.php?id=<?= urlencode($data["id"]) ?>">
-                              <i class="bi bi-pencil-square">Ubah</i>
-                            </a>
+                            <a class="btn app-btn-primary p-2" href="edit.php?id=<?= urlencode($data["id"]) ?>">Edit</a>
                             <a class="btn app-btn-secondary deleteButton p-2" href="delete.php?id=<?= urlencode($data['id']) ?>">Hapus</a>
                           </td>
                         </tr>
