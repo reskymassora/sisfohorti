59659340d2a0ebd7279090237eec79a4ed39bb39
reskymassora <reskymassora@gmail.com>
2024-08-01 09:50:31 +0800

    mengembalikan file index

--- index.php
+++ index.php
@@ -114,7 +114,6 @@
 
 </body>
 
-
 <!-- jQuery, Popper.js, and Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
