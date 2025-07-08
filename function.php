2682cb7385536ccc43d4acf597f346baf15ab580
reskymassora <reskymassora@gmail.com>
2025-07-08 17:45:59 +0800

    error koneksi

--- function.php
+++ function.php
@@ -5,7 +5,7 @@
 // $dbname = "u832397905_simfonidb"; 
 
 $servername = "localhost";
-$username = "u832397905_R35ky";
+$username = "u832397905_R35k";
 $password = "@R35kym4550r4";
 $dbname = "u832397905_simfonidb"; 
 
